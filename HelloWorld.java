c9ac1981b23ca3d09518e44269e4382705c33f3c
vickymahour <vicky.mahour@gmail.com>
2024-06-23 19:13:09 +0530

    code without error free

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
+import java.sql.Date;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("This is basic java program");
     }
 }
